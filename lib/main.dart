a2306ea5d008b69728f0bd49c4f1de9f8f24b1d7
msdhanga <mohammaddhanga2@gmail.com>
2023-04-29 17:18:15 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,59 @@ class Login_app extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
           child: Center(
-            child: CircleAvatar(
-              radius: 95,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+            CircleAvatar(
+              radius: 55,
               backgroundImage: AssetImage('assets/Image/login.png'),
             ),
+               Container(
+                 margin: EdgeInsets.symmetric(horizontal: 30 , vertical: 20),
+                 child: TextFormField(
+                   decoration: InputDecoration(
+                   fillColor: Colors.black26,
+                   labelText: 'Enter Number',
+                   filled: true,
+                   ),
+                   keyboardType: TextInputType.phone,
+                 ),
+               ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30 ,vertical: 07),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      fillColor: Colors.black38,
+                      labelText: 'Password',
+                      filled: true,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent , fixedSize: Size(150,20)),
+                  onPressed: ()=>{},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 22),
+              ),
+             )
+           ],
           ),
         ),
       ),
+    ),
     );
   }
 }
